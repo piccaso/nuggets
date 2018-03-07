cc56c44d98d9101e2106fa13510e61ec158b8432
Florian Fida <f+git@fida.biz>
2018-03-07 23:46:38 +0100

    CsvHelper.Ext

---
+++ CsvHelper.Ext.Test/CsvHelperExtTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using NUnit.Framework;
+
+namespace CsvHelper.Ext.Test
+{
+    [TestFixture]
+    public class CsvHelperExtTests
+    {
+        [Test]
+        public void ReadTest1()
+        {
+            var file = DateTime.Now.Ticks.ToString() + ".csv";
+            const string content = "Col1;Col2;Col3\r\n" +
+                                   "a;1;1.22\r\n" +
+                                   "b;2;2.33\r\n";
+            File.WriteAllText(file, content, ExcelCsv.GetExcelAtDeConfig().Encoding);
+
+            var res = ExcelCsv.ReadCsv(file, row => new
+            {
+                c1 = row[0],
+                c2 = row.GetField<int>("Col2"),
+                c3 = Decimal.Parse(row[2], CultureInfo.InvariantCulture),
+            });
+
+            foreach (var r in res)
+            {
+                Console.WriteLine($"{r.c1};{r.c2};{r.c3}");
+            }
+
+        }
+
+        [Test]
+        public void Bytes()
+        {
+            dynamic line1 = new System.Dynamic.ExpandoObject();
+            line1.x = 1;
+            line1.y = 2;
+
+            dynamic line2 = new System.Dynamic.ExpandoObject();
+            line2.x = "a";
+            line2.y = "b";
+
+            var data = new List<dynamic>{line1, line2};
+
+            var bytes = data.GetCsvBytes();
+            var str = ExcelCsv.GetExcelAtDeConfig().Encoding.GetString(bytes).Replace("\n", "").Replace("\r", "");
+            Console.WriteLine(str);
+            Assert.AreEqual("x;y1;2a;b", str);
+        }
+    }
+}
---
+++ CsvHelper.Ext/ExcelCsv.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CsvConfig = CsvHelper.Configuration.Configuration;
+
+namespace CsvHelper.Ext
+{
+    public static class ExcelCsv
+    {
+        public static CsvConfig GetExcelAtDeConfig() => new CsvConfig
+        {
+            Delimiter = ";",
+            Encoding = Encoding.GetEncoding(1252),
+        };
+
+        public static IList<T> ReadCsv<T>(string fileName, CsvConfig config = null)
+        {
+            config = config ?? GetExcelAtDeConfig();
+            using (var fs = File.OpenRead(fileName))
+            using (var sr = new StreamReader(fs, config.Encoding))
+            {
+                var csvReader = new CsvReader(sr, config);
+                return csvReader.GetRecords<T>().ToList();
+            }
+        }
+
+        public static IList<T> ReadCsv<T>(string fileName, Func<IReaderRow, T> readFunc) => ReadCsv(fileName, GetExcelAtDeConfig(), readFunc);
+        public static IList<T> ReadCsv<T>(string fileName, CsvConfig config, Func<IReaderRow,T> readFunc)
+        {
+            config = config ?? GetExcelAtDeConfig();
+            using (var fs = File.OpenRead(fileName))
+            using (var sr = new StreamReader(fs, config.Encoding))
+            {
+                var records = new List<T>();
+                var csvReader = new CsvReader(sr, config);
+                if (config.HasHeaderRecord)
+                {
+                    csvReader.Read();
+                    csvReader.ReadHeader();
+                }
+
+                while (csvReader.Read())
+                {
+                    records.Add(readFunc(csvReader));
+                }
+
+                return records;
+            }
+        }
+
+        public static void WriteCsvTo(this System.Collections.IEnumerable data, Stream stream, CsvConfig config = null)
+        {
+            config = config ?? GetExcelAtDeConfig();
+            using (var sw = new StreamWriter(stream, config.Encoding))
+            {
+                var csvWriter = new CsvWriter(sw, config);
+                csvWriter.WriteRecords(data);
+            }
+        }
+        public static void WriteCsv(this System.Collections.IEnumerable data, string filename, CsvConfig config = null)
+        {
+            using (var fs = File.OpenWrite(filename))
+            {
+                data.WriteCsvTo(fs, config);
+            }
+        }
+        public static byte[] GetCsvBytes(this System.Collections.IEnumerable data, CsvConfig config = null)
+        {
+            using (var ms = new MemoryStream())
+            {
+                data.WriteCsvTo(ms, config);
+                return ms.ToArray();
+            }
+        }
+    }
+}
